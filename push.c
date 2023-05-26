58bd55f229d1a383eefcb8206c7794d16feb57d5
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-26 11:53:26 +0000

    fix betty error

--- push.c
+++ push.c
@@ -4,33 +4,34 @@
  * push - Pushes an element to the stack.
  * @stack: Double pointer to the head of the stack.
  * @line_number: The line number being executed from the Monty file.
+ * @arg: the value to be pushed
  */
 void push(stack_t **stack, char *arg, unsigned int line_number)
 {
-    int value;
-    stack_t *new_node;
+	int value;
+	stack_t *new_node;
 
-    if (!arg || !isdigit((unsigned char)*arg))
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!arg || !isdigit((unsigned char)*arg))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    value = atoi(arg);
+	value = atoi(arg);
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
 
-    *stack = new_node;
+	*stack = new_node;
 }
