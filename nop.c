7ce48ec8c169da3164acd403c3234ecc1a56b420
jeshi2 <jamesvean19@gmail.com>
2023-05-26 12:21:25 +0300

    jeshi

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: The line number being executed from the Monty file.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
