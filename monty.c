c59d2f66deca577d03dbdd01b74c5983a470d821
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-26 11:52:01 +0000

    Fix betty error

--- monty.c
+++ monty.c
@@ -1,58 +1,67 @@
 #include "monty.h"
 
+/**
+ * main - Begin execution
+ * @argc: number of arugument passed
+ * @argv: array of arguments
+ *
+ * Return: 0 on successful
+ */
 int main(int argc, char *argv[])
 {
-    char *filename, *opcode, *value;
-    stack_t *stack = NULL;
-    FILE *file;
-    char line[256];
-    unsigned int line_number = 1;
+	char *filename, *opcode, *value;
+	stack_t *stack = NULL;
+	FILE *file;
+	char line[256];
+	unsigned int line_number = 1;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
-
-    filename = argv[1];
-    file = fopen(filename, "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        return EXIT_FAILURE;
-    }
-
-       
-    while (fgets(line, sizeof(line), file))
-    {
-        line[strcspn(line, "\n")] = '\0';  /*Remove newline character*/
-
-        /* Split line into tokens*/
-        opcode = strtok(line, " ");
+	if (argc != 2)
+		print_error_messages(3, NULL, NULL, 0);
+	filename = argv[1];
+	file = fopen(filename, "r");
+	if (!file)
+		print_error_messages(4, NULL, filename, 0);
+	while (fgets(line, sizeof(line), file))
+	{
+		line[strcspn(line, "\n")] = '\0';  /*Remove newline character*/
+		opcode = strtok(line, " ");/* Split line into tokens*/
 		value = strtok(NULL, " ");
 		if (strtok(NULL, " "))
+			print_error_messages(1, NULL, NULL, line_number);
+		if (opcode)
 		{
-        	fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        	exit(EXIT_FAILURE);
-    	}
+			if (strcmp(opcode, "push") == 0)
+				push(&stack, value, line_number);
+			else if (strcmp(opcode, "pall") == 0)
+				pall(&stack, line_number);
+			else
+				print_error_messages(2, file, opcode, line_number);
+		}
+		line_number++;
+	}
+	fclose(file);
+	return (0);
+}
 
-        if (opcode)
-        {
-            if (strcmp(opcode, "push") == 0)
-                push(&stack, value, line_number);
-            else if (strcmp(opcode, "pall") == 0)
-                pall(&stack, line_number);
-            else
-            {
-                fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-                fclose(file);
-                return EXIT_FAILURE;
-            }
-        }
-
-        line_number++;
-    }
-
-    fclose(file);
-    return 0;
+void print_error_messages(int num, FILE *file,
+		char *filename_or_opcode, unsigned int line_number)
+{
+	switch (num)
+	{
+		case 1:
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			break;
+		case 2:
+			fprintf(stderr, "L%u: unknown instruction %s\n",
+					line_number, filename_or_opcode);
+			fclose(file);
+			break;
+		case 3:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 4:
+			fprintf(stderr, "Error: Can't open file %s\n", filename_or_opcode);
+			break;
+	}
+	exit(EXIT_FAILURE);
 }
