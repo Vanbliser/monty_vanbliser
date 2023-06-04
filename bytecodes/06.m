7686a607d8514a09ada96bf34a292c38eeb89b19
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-04 08:29:55 +0000

    Fix merge conflict

--- bytecodes/06.m
+++ bytecodes/06.m
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 push 1
 push 2
 push 10
 push 3
 sub
 pall
-=======
-
-
-
-			
-
-
-			
-					
->>>>>>> c0e73d56c7c789b9199c515958a7f9e53f5fd2c5
--- bytecodes/13.m
+++ bytecodes/13.m
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-push 1
-push 2
-push 3
-pall
-rotr
-=======
 push 0
 push 1
 push 2
@@ -13,5 +6,4 @@ push 2
 push 4
     push 5    
       push    6        
->>>>>>> c0e73d56c7c789b9199c515958a7f9e53f5fd2c5
 pall
